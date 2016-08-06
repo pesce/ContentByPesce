e2aaded700407c9a0ce7988090fe49d49deab2dc
pesce <pesce@pescetech.com>
2016-08-05 20:02:12 -0700

    slight file delete attack security

--- admin/delete.php
+++ admin/delete.php
@@ -1,5 +1,8 @@
 <?php
 require('config.php');
 $file = $_POST['filename'];
+$file = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $file);
+$file = mb_ereg_replace("([\.]{2,})", '', $file);
+
 rename($FilePath.$file, $TrashPath.$file);
 ?>
